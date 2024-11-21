476d918fc2eae77e3973a65dfddf146d80d2cebb
Calderara Arianna (Student Com20) <arianna.calderara@stud-inf.unibz.it>
2024-11-21 17:54:15 +0100

    update dataset.py and create notebook.ipynb

---
+++ src/dataset.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+data = {
+    "Name": ["A", "B", "C", "D", "E"],
+    "Age": [22, 19, 54, 67, 89],
+    "City": ["Imola", "Roma", "Milano", "Firenze", "Bologna "],
+}
+df = pd.DataFrame(data)
